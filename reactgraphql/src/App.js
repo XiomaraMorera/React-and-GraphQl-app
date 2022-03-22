c46c9f11d5b8261ec7508e76a083fcf44d5a3ff3
XiomaraMorera <x.morera@arkoselabs.com>
2022-03-22 13:27:31 +1000

    Updating queryies and components

--- reactgraphql/src/App.js
+++ reactgraphql/src/App.js
@@ -1,13 +1,87 @@
 
+import github from "./db";
+import {useEffect, useState, useCallback} from "react";
+import query from './Query';
+import RepoInfo from "./RepoInfo";
+
+
 
 function App() {
+
+  const [userName, setUserName] = useState("");
+
+  const [repoList, setRepoList] = useState(null);
+
+  const fetchData = useCallback(()=> {
+
+    const queryText = JSON.stringify(query);
+    
+    fetch(github.baseURL, {
+      
+      method: "POST",
+      
+      headers: github.headers,
+      
+      body: queryText
+    
+    })
+    
+    .then(res=> res.json())
+    
+    .then(data=> {
+      
+      const viewer = data.data.viewer;
+      
+      const repos = data.data.search.nodes;
+      
+      setUserName(viewer.name);
+      
+      setRepoList(repos);
+      
+      console.log(data);
+    
+    })
+    
+    .catch((err)=> {
+     
+      console.log(err)
+    
+    });
+  
+  }, []);
+  
+  useEffect(()=> {
+    
+    fetchData();
+  
+  }, [fetchData]);
+  
   return (
+    
     <div className="App container mt-5">
+
       <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i> Repos</h1>
 
-   
+      <p> Hey there {userName}</p>
+
+      { repoList && (
+
+          <ul className="list-group list-group-flush">
+
+            { repoList.map((repo) => (
+
+                <RepoInfo key={repo.id} repo={repo} />
+
+            ))}  
+                  
+          </ul>
+
+      )}
+
     </div>
+
   );
-}
+
+};
 
 export default App;
---
+++ reactgraphql/src/Query.js
@@ -0,0 +1,30 @@
+const githubQuery = {
+    query: `
+    {
+        viewer {
+          name
+        }
+        search(
+          query: "user:xiomaramorera sort:updated-desc"
+          type: REPOSITORY,
+          first: 10
+        ) {
+          nodes {
+            ... on Repository {
+              name
+              description
+              id
+              url
+              viewerSubscription
+              licenseInfo {
+                spdxId
+              }
+            }
+          }
+        }
+      }
+    `,
+  };
+
+export default githubQuery;
+
